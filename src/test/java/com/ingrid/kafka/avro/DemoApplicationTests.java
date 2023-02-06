eb940b257f428a90cfbc4ab3b12844faa5edd9ce
ingridbromer <ingridb@id.uff.br>
2023-02-06 14:37:49 -0300

    kafka-avro producer&consumer configs

---
+++ src/test/java/com/ingrid/kafka/avro/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ingrid.kafka.avro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
